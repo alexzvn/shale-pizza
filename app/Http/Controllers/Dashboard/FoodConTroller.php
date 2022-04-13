84dce6d32492bc4993f0d57b32990815f99a7269
minhduc <ducpbmgch200146@fpt.edu.vn>
2022-04-13 17:02:41 +0700

    Manage Foods

---
+++ app/Http/Controllers/Dashboard/FoodConTroller.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodConTroller extends Controller
+{
+    public function index()
+    {
+        return view('dashboard.foods.index',[
+            'foods'=> Food::paginate()
+        ]);
+    }
+
+    public function create(){
+        return view('dashboard.foods.create');
+    }
+
+    public function store(Request $request, Food $food){
+        $food->fill(
+            $this->validate($request, $this->rules())
+        );
+
+        $food->save();
+
+        return to_route('manager.foods');
+    }
+
+    public function edit(Food $food){
+        return view('dashboard.foods.edit',[
+            'food'=>$food
+        ]);
+    }
+
+    public function update(Request $request, Food $food){
+        $attributes = $this->validate($request, [
+            'name'=>'nullable|string|max:255',
+            'price' => 'nullable|numeric|gte:0',
+            'image' => 'nullable|string|max:2048',
+            'description' => 'nullable'
+        ]);
+
+        $food->fill($attributes);
+    }
+
+    public function delete(Food $food){
+        $food->delete();
+
+        return to_route('manager.foods');
+    }
+    
+    public function rules(){
+        return[
+            'name'=>'required|string|max:255',
+            'price' => 'required|numeric|gte:0',
+            'image' => 'nullable|string|max:2048',
+            'description' => 'nullable'
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'foods'],function(){
+    Route::get('', [FoodConTroller::class, 'index'])->name('manager.foods');
+    Route::get('create',[FoodConTroller::class, 'create'])->name('manager.foods.create');
+    Route::post('store',[FoodConTroller::class, 'store'])->name('manager.foods.store');
+    Route::get('{food}/edit',[FoodConTroller::class, 'edit'])->name('manager.foods.edit');
+    Route::post('{food}/update',[FoodConTroller::class, 'update'])->name('manager.foods.update');
+    Route::delete('{food}/delete',[FoodConTroller::class, 'delete'])->name('manager.foods.delete');
+});
