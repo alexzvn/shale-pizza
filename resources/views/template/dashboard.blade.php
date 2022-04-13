e6b2caa35750036a3f38026a20d34041bc8aec55
Alexzvn <boytunghc@gmail.com>
2022-04-13 16:33:08 +0700

    add links to dashboard

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -23,34 +23,11 @@
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
               <li class="nav-item">
-                <a href="#" class="nav-link active" aria-current="page">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-                  Home
-                </a>
+                <a href="{{ route('manager') }}" class="nav-link" aria-current="page">Home</a>
               </li>
-              <li>
-                <a href="#" class="nav-link link-dark">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link link-dark">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-                  Orders
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link link-dark">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                  Products
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link link-dark">
-                  <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-                  Customers
-                </a>
+
+              <li class="nav-item">
+                <a href="{{ route('manager.admin') }}" class="nav-link" aria-current="page">Admin</a>
               </li>
             </ul>
             <hr>
