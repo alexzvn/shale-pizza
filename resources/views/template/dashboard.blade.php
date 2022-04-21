fd8e91c4e073b1a9562e057799c96a0558018435
Alexzvn <boytunghc@gmail.com>
2022-04-21 10:16:33 +0700

    add form to logout

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -66,9 +66,9 @@
                             </a>
                         </div>
                         <div class="">
-                            <form class="dropdown-item" action="{{ route('logout') }}" method="POST">
+                            <form id="form-logout" class="dropdown-item" action="{{ route('logout') }}" method="POST">
                                 @csrf
-                                <a class="" href="auth_login.html">
+                                <a href="javascript:void(0)" onclick="document.querySelector('#form-logout').submit()">
                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
                                   Sign Out
                               </a>
